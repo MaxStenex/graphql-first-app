52929095c92c2ac3d80c83f8d353c3cd78a1a598
Maxim Rozinkevich <yourstenex@gmail.com>
2020-11-29 15:39:57 +0300

    added apollo-client

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
 import "./styles/App.css";
+import { ApolloClient, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+});
 
 const App = () => {
-  return <h1>Hello world</h1>;
+  return (
+    <ApolloProvider client={client}>
+      <h1>Hello world</h1>
+    </ApolloProvider>
+  );
 };
 
 export default App;
